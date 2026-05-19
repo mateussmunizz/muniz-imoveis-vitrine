5b2e30752977df61ab0f59895c275ea161d2f1d1
Mateus Muniz <mateus.muniz2013@gmail.com>
2026-05-19 19:17:30 -0300

    Adicionando handler GET e POST no NextAuth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,3 +10,6 @@ export const authOptions = {
   ],
   secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
 };
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
